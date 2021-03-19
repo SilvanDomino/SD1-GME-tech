1e531a2d36a21fd8a68d536586fcdcaa60d13e2f
SilvanDomino <silvah489@gmail.com>
2021-03-19 10:18:59 +0100

    initial commit

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float jumpPower = 8;
+
+    void Start()
+    {
+        Debug.Log("Hello World");
+
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.velocity = new Vector3(0, jumpPower, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/PrefabSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject go = Instantiate(prefab, new Vector3(0, 7, 0), Quaternion.identity);
+            Destroy(go, 3.0f);
+            go.name = "Bullet-ish object";
+            Rigidbody rb = go.GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(10, 0, 0);
+        }
+    }
+}
