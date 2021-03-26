6d2bb14ba05bb9b73d6003cffa76a9228d16619a
SilvanDomino <silvah489@gmail.com>
2021-03-26 09:31:12 +0100

    Coin added and double jump removed

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -6,20 +6,29 @@ public class Jump : MonoBehaviour
 {
     private Rigidbody rb;
     public float jumpPower = 8;
+    public bool canJump = true;
 
     void Start()
     {
-        Debug.Log("Hello World");
-
         rb = gameObject.GetComponent<Rigidbody>();
     }
 
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if ( Input.GetKeyDown(KeyCode.W) && canJump == true)
         {
             rb.velocity = new Vector3(0, jumpPower, 0);
         }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        canJump = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        canJump = false;
+    }
+
 }
