f84df4a0b369fb1078c9708498cbab9361ac7a2b
SilvanDomino <silvah489@gmail.com>
2022-03-18 11:24:31 +0100

    added animations. Coin gets picked up

---
+++ Assets/Scripts/PickupCoin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupCoin : MonoBehaviour
+{
+    public GameObject prefab;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Coin")
+        {
+            GameObject go = Instantiate(prefab, other.transform.position, Quaternion.identity);
+            Destroy(go, 1);
+            Destroy(other.gameObject);
+
+        }
+    }
+}
